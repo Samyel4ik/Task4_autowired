15971db82844c759558f9f5ea6a9d0b2df92c4fc
Evgen <y.samasiyk@astondevs.ru>
2024-08-13 22:44:58 +0300

    first push

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,6 +1,7 @@
 package koschei.config;
 
 import koschei.models.Island2;
+import koschei.models.Rabbit4;
 import koschei.models.Wood3;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,7 +12,8 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
 
     @Bean
-    public static Island2 getIsland(Wood3 wood) {
+    public  Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
     }
+
 }
